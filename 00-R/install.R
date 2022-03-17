4192b2e9b62a246ea1fcd146d8c4f43f0ac16f73
Noah Siegel <nasiegel@ucdavis.edu>
2022-03-17 16:42:40 -0700

    Update install.R

--- 00-R/install.R
+++ 00-R/install.R
@@ -2,7 +2,6 @@ install.packages('MazamaSpatialUtils')
 install.packages('PWFSLSmoke')
 install.packages('knitr')
 install.packages('rmarkdown')
-install.packages('MazamaSpatialUtils')
 install.packages('tidyverse')
 install.packages('dplyr')
 
