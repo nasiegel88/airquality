457856b01490c65836acaab8e5a9b09ac7dfd995
Noah Siegel <nasiegel@ucdavis.edu>
2022-03-17 16:39:37 -0700

    Update packages.R

--- 00-R/packages.R
+++ 00-R/packages.R
@@ -1,9 +1,16 @@
-# Install packages not listed on conda
-if(!require(MazamaSpatialUtils)){install.packages("MazamaSpatialUtils");library("MazamaSpatialUtils")}
-if(!require(devtools)){install.packages("devtools");library("devtools")}
-if(!require(sf)){install.packages("sf");library("sf")}
-if(!require(leaflet)){install.packages("leaflet");library("leaflet")}
-if(!require(raster)){install.packages("https://cran.r-project.org/src/contrib/Archive/raster/raster_2.5-8.tar.gz", repos = NULL, type="source");library("raster")}
-if(!require(rlang)){install.packages("rlang");library("rlang")}
+## Install packages not listed on conda
+#if(!require(MazamaSpatialUtils))#{install.packages("MazamaSpatialUtils");library("MazamaSpatialUt#ils")}
+#if(!require(devtools))#{install.packages("devtools");library("devtools")}
+#if(!require(sf)){install.packages("sf");library("sf")}
+#if(!require(leaflet))#{install.packages("leaflet");library("leaflet")}
+#if(!require(raster)){install.packages("https://cran.r-#project.org/src/contrib/Archive/raster/raster_2.5-8.tar.gz", #repos = NULL, type="source");library("raster")}
+#if(!require(rlang)){install.packages("rlang");library("rlang")}
+#
+#if(!require(PWFSLSmoke))#{install.packages("PWFSLSmoke");library("PWFSLSmoke")}
 
-if(!require(PWFSLSmoke)){install.packages("PWFSLSmoke");library("PWFSLSmoke")}
+library('MazamaSpatialUtils')
+library('PWFSLSmoke')
+library('knitr')
+library('rmarkdown')
+library('MazamaSpatialUtils')
+library('tidyverse')
