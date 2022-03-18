07586995d15b984416a503d92348c1b1f9c9a6b3
Noah Siegel <nasiegel@ucdavis.edu>
2022-03-18 12:26:29 -0700

    Update packages.R

--- 00-R/packages.R
+++ 00-R/packages.R
@@ -1,5 +1,10 @@
 # Install packages not listed on conda
+read.lib <- list(
+  'tidyverse',
+  'devtools'
+)
+lapply(read.lib, function(x) {suppressPackageStartupMessages(library(x))})
 
-if(!require(MazamaSpatialUtils)){suppressPackageStartupMessages(install_version("MazamaSpatialUtils", "0.7.6"));library("MazamaSpatialUtils")}
+if(!require(MazamaSpatialUtils)){install_version("MazamaSpatialUtils", "0.7.6");suppressPackageStartupMessages(library("MazamaSpatialUtils"))}
 
-if(!require(PWFSLSmoke)){suppressPackageStartupMessages(install_version("PWFSLSmoke", "1.2.117"));library("PWFSLSmoke")}
+if(!require(PWFSLSmoke)){install_version("PWFSLSmoke", "1.2.117");suppressPackageStartupMessages(library("PWFSLSmoke"))}
