fb8be69e224c8865960c61dde4de88e67899f540
Noah Siegel <nasiegel@ucdavis.edu>
2022-03-18 12:39:28 -0700

    Update packages.R

--- 00-R/packages.R
+++ 00-R/packages.R
@@ -1,9 +1,8 @@
 # Install packages not listed on conda
-read.lib <- list(
-  'tidyverse',
-  'devtools'
-)
-lapply(read.lib, function(x) {suppressPackageStartupMessages(library(x))})
+
+suppressPackageStartupMessages(library("tidyverse"))
+
+suppressPackageStartupMessages(library("devtools"))
 
 if(!require(MazamaSpatialUtils)){install_version("MazamaSpatialUtils", "0.7.6");suppressPackageStartupMessages(library("MazamaSpatialUtils"))}
 
