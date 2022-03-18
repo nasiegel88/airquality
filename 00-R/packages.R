f79609d79015ae3b65834e58ad1dc7e20b3ab26c
Noah Siegel <nasiegel@ucdavis.edu>
2022-03-18 08:16:03 -0700

    Update packages.R

--- 00-R/packages.R
+++ 00-R/packages.R
@@ -1,5 +1,5 @@
 # Install packages not listed on conda
 
-if(!require(azamaSpatialUtils)){install_version("MazamaSpatialUtils", "0.7.6");library("MazamaSpatialUtils")}
+if(!require(MazamaSpatialUtils)){install_version("MazamaSpatialUtils", "0.7.6");library("MazamaSpatialUtils")}
 
 if(!require(PWFSLSmoke)){install_version("PWFSLSmoke", "1.2.117");library("PWFSLSmoke")}
