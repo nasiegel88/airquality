919163645000fc023132af81274c1111be977833
Noah Siegel <nasiegel@ucdavis.edu>
2022-03-18 08:13:23 -0700

    Update packages.R

--- 00-R/packages.R
+++ 00-R/packages.R
@@ -1,8 +1,5 @@
 # Install packages not listed on conda
-if(!require(azamaSpatialUtils)){install.packages("MazamaSpatialUtils");library("MazamaSpatialUtils")}
-if(!require(PWFSLSmoke)){install.packages("PWFSLSmoke");library("PWFSLSmoke")}
-#if(!require(devtools)){install.packages("devtools");library("devtools")}
-#if(!require(sf)){install.packages("sf");library("sf")}
-#if(!require(leaflet)){install.packages("leaflet");library("leaflet")}
-#if(!require(raster)){install.packages("https://cran.r-#project.org/src/contrib/Archive/raster/aster_2.5-8.tar.gz", repos = NULL, type="source");library("raster")}
-#if(!require(rlang)){install.packages("rlang");library("rlang")}
+
+if(!require(azamaSpatialUtils)){install_version("MazamaSpatialUtils", "0.7.6");library("MazamaSpatialUtils")}
+
+if(!require(PWFSLSmoke)){install_version("PWFSLSmoke", "1.2.117");library("PWFSLSmoke")}
