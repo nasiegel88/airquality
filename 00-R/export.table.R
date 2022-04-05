abeb0406939bb17b0583582c7d210148a190d607
nasiegel88 <nasiegel@ucdavis.edu>
2022-04-05 12:07:21 -0700

    txt to tsv

--- 00-R/export.table.R
+++ 00-R/export.table.R
@@ -4,7 +4,7 @@ function(table, filename) {
   suppressPackageStartupMessages(library(tidyverse))
   dir.create(file.path(getwd(), "01-Results"))
   file <- file.path(getwd(), "01-Results", filename)
-  write.table(table, file = paste0(file, ".txt"), quote = F,
+  write.table(table, file = paste0(file, ".tsv"), quote = F,
               sep = "\t", row.names = F)
   openxlsx::write.xlsx(table, paste0(file, ".xlsx"), rowNames=TRUE)
 }
