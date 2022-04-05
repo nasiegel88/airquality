806a7ba7d5e1cffe0f5d85017a596132a7f0e607
nasiegel88 <nasiegel@ucdavis.edu>
2022-04-04 20:37:30 -0700

    update plot

--- 00-R/fire_plot_aqi.R
+++ 00-R/fire_plot_aqi.R
@@ -1,23 +1,17 @@
 fire_plot_aqi <-
 function(x, title) {
-
-data <- x$data %>% na.omit()
-names(data)[2] <- "AQI"
-data$datetime <- as.POSIXct(data$datetime, format="%Y-%m-%d %H:%M:%S") 
-p <- data %>%
-    ggplot(aes(x=datetime, y=AQI)) +
-        geom_line() +
-        scale_x_datetime(
-            date_breaks = "3 day",
-            date_labels = "%b %d"
-        ) + 
-labs(caption = "", x = "Day", y = "AQI") 
-
-# parameters
-p <- p + theme_classic() +
-    theme(plot.caption = element_text(face = "italic"),
-          plot.subtitle = element_text(face = "bold"),
-          text = element_text(size=20)) 
-p <- p + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5))
+    # Make plot
+    p <- x %>% ggplot(aes_string(x = 'Date.time', y = eval(names(data)[2]))) +
+    geom_line() + 
+    scale_x_datetime(date_breaks = "3 day", date_labels = "%b %d") + 
+    labs(caption = "", x = "Day", y = bquote(.(names(data)[2])~'('*ug/m^3*')'))
+    # Increase font
+    p <- p + 
+        theme_classic() + 
+        theme(plot.caption = element_text(face = "italic"), plot.subtitle = element_text(face = "bold"), text = element_text(size = 20))
+    # Addd title
+    p <- p + 
+        ggtitle(title) + 
+        theme(plot.title = element_text(hjust = 0.5))
 p
 }
