d0ed6f7ba68bf0b1e4f2c04cbd2a1c3aea80cf24
nasiegel88 <nasiegel@ucdavis.edu>
2022-04-05 08:27:56 -0700

    fix

--- 00-R/fire_plot_aqi.R
+++ 00-R/fire_plot_aqi.R
@@ -1,7 +1,8 @@
 fire_plot_aqi <-
 function(x, title) {
     # Make plot
-    p <- x %>% ggplot(aes_string(x = 'Date.time', y = eval(names(data)[2]))) +
+    var <- names(x)[2]
+    p <- x %>% ggplot(aes_string(x = 'Date.time', y = var)) +
     geom_line() + 
     scale_x_datetime(date_breaks = "3 day", date_labels = "%b %d") + 
     labs(caption = "", x = "Day", y = bquote(.(names(data)[2])~'('*ug/m^3*')'))
