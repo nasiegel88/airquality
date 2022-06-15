702d625702d463e790dbfc72be01426696e88a61
Noah Siegel <nasiegel@ucdavis.edu>
2022-06-15 10:09:49 -0700

    Delete 00-R directory

--- 00-R/export.plot.R
+++
@@ -1,8 +0,0 @@
-export.plot <-
-function(plot, filename) {
-  suppressPackageStartupMessages(library(openxlsx))
-  suppressPackageStartupMessages(library(tidyverse))
-    dir.create(file.path(getwd(), "01-Results"))
-    file <- file.path(getwd(), "01-Results", paste(filename , ".png", sep=""))
-    ggsave(filename= file, plot=plot, width=18, height=8, dpi=600)
-}
